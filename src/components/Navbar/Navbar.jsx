116a4933141b913929eee0e1bab312f6ceb66966
wacho91 <criskol.71@gmail.com>
2024-10-01 11:03:19 -0500

    Lgoo complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,23 @@
-
+import Logo from "../../assets/website/logo.png"
 
 const Navbar = () => {
   return (
     <div>
-        Navbar Components
+        <div className="container">
+            <div className="flex justify-between items-center">
+                <div>
+                    <a href="#" className="font-extrabold text-2xl sm:text-3xl flex gap-2">
+                        <img src={Logo} alt="" className="w-10" />
+                        Books
+                    </a>
+                </div>
+                <div>
+                    <ul>
+                        <li>Home</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
