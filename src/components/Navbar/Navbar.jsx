1eddfcc9e23b17580de8674ed692e3d2a8b1ddbd
wacho91 <criskol.71@gmail.com>
2024-10-01 11:09:29 -0500

    Items for navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import Logo from "../../assets/website/logo.png"
 
+const Menu = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "Best Seller",
+      link: "/#services",
+    },
+];
+
 const Navbar = () => {
   return (
     <div>
-        <div className="container">
+        <div className="container py-3 sm:py-0">
             <div className="flex justify-between items-center">
                 <div>
                     <a href="#" className="font-extrabold text-2xl sm:text-3xl flex gap-2">
@@ -13,7 +26,13 @@ const Navbar = () => {
                 </div>
                 <div>
                     <ul>
-                        <li>Home</li>
+                        {
+                            Menu.map((menu) =>  (
+                                <li key={menu.id}>
+                                    <a href={menu.link} className="text-lg sm:text-xl font-bold hover:text-blue">{menu.name}</a>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
             </div>
