8cb4c6269d75f5453cd9c083b0ed4758fbb0dea3
wacho91 <criskol.71@gmail.com>
2024-10-04 11:14:25 -0500

    Hero Component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+
+const Hero = () => {
+  return (
+    <div 
+        className="min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center 
+        dark:bg-gray-950 dark:text-white duration-200"
+    >
+        <div className="container pb-8 sm:pb-0">
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
