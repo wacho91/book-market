6ed81f28955ebd03a748d22e8e1bba0218b0d1cf
wacho91 <criskol.71@gmail.com>
2024-10-04 10:43:00 -0500

    Inserting mode light

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
 
+import DarkPng from "../../assets/website/dark-mode-button.png"
+import LightPng from "../../assets/website/light-mode-button.png"
 
 const DarkMode = () => {
   return (
-    <div>
-        DarkMode Component
+    <div className="">
+      <img 
+        src={LightPng} 
+        alt=""  
+        className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] 
+        transition-all hover:drop-shadow-[1px_1px_1px_rgba(0,0,0,0.5)]"
+      />
+      {/* <img src={DarkPng} alt="" className="w-12"/> */}
     </div>
   )
 }
