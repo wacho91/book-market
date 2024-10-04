a210e4a6b8a9c9c571fb6609999dd160a353c804
wacho91 <criskol.71@gmail.com>
2024-10-04 10:58:56 -0500

    Funcionality mode complete

--- src/components/Navbar/DarkMode.jsx
+++ src/components/Navbar/DarkMode.jsx
@@ -1,18 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import DarkPng from "../../assets/website/dark-mode-button.png"
 import LightPng from "../../assets/website/light-mode-button.png"
 
 const DarkMode = () => {
+
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  useEffect(() => {
+    if(theme === "dark") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme])
+
   return (
-    <div className="">
+    <div className="relative">
       <img 
         src={LightPng} 
         alt=""  
-        className="w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] 
-        transition-all hover:drop-shadow-[1px_1px_1px_rgba(0,0,0,0.5)]"
+        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] 
+        transition-all duration-300 absolute right-0 z-10  ${theme === "dark" ? "opacity-0" : "opacity-100"}
+        `}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       />
-      {/* <img src={DarkPng} alt="" className="w-12"/> */}
+      <img 
+        src={DarkPng} 
+        alt="" 
+        className="w-12 cursor-pointer drop-shadow-[1px_1px_2px_rgba(0,0,0,0.5)] duration-300"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      />
     </div>
   )
 }
