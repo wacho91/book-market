99f9d13916ebc5050708004b9bc4a3268c91ede3
wacho91 <criskol.71@gmail.com>
2024-10-07 11:37:37 -0500

    Section Title Book

--- src/components/BestBook/BestBook.jsx
+++ src/components/BestBook/BestBook.jsx
@@ -2,14 +2,15 @@
 
 const BestBook = () => {
   return (
-    <div>
+    <div className="py-10">
         <div className="container">
-            <div>
-                <p>
+            {/*Header Section*/}
+            <div className="text-center mb-20 max-w-[400px] mx-auto">
+                <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
                     Trending Books 
                 </p>
-                <h1>Besto Books</h1>
-                <p>
+                <h1 className="text-3xl font-bold">Besto Books</h1>
+                <p className="text-xs text-gray-400">
                     Lorem ipsum, dolor sit amet 
                     consectetur adipisicing elit. Expedita delectus 
                     impedit recusandae{" "}
