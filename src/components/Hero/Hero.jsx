ecc25b689a3a21411969f2a3c2934858c75bad07
wacho91 <criskol.71@gmail.com>
2024-10-07 10:11:38 -0500

    Changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,18 @@
 
 const Hero = () => {
   return (
-    <div 
-        className="min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center 
-        dark:bg-gray-950 dark:text-white duration-200"
-    >
+    <div className="min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
         <div className="container pb-8 sm:pb-0">
             <div className="grid grid-cols-1 sm:grid-cols-2">
                 {/*Text Content Section*/}
-                <div className="text-5xl sm:text-6xl lg:text-7xl font-bold">
-                    <h1>Hero</h1>
-                    <p className="bg-clip-text text-transparent bg-gradient-to-b from-primary text-right text-sm to-secondary">By Anonimous</p>
+                <div>
+                    <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                        Hero
+                        <p className="bg-clip-text text-transparent bg-gradient-to-b 
+                        from-primary text-right text-sm to-secondary">
+                            by anonymous
+                        </p>
+                    </h1>
                     <p className="text-sm">
                         Lorem ipsum, dolor sit amet 
                         consectetur adipisicing elit. 
@@ -32,5 +34,6 @@ const Hero = () => {
     </div>
   )
 }
+
 
 export default Hero
