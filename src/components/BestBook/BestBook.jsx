b9bafceb2cbaea1ffc0c761223bbfcb509c86501
wacho91 <criskol.71@gmail.com>
2024-10-07 11:48:20 -0500

    Content component

--- src/components/BestBook/BestBook.jsx
+++ src/components/BestBook/BestBook.jsx
@@ -1,4 +1,31 @@
 
+import Img1 from "../../assets/book/book1.jpg"
+import Img2 from "../../assets/book/book2.jpg"
+import Img3 from "../../assets/book/book3.jpg"
+
+const BooksData = [
+    {
+      id: 1,
+      img: Img1,
+      title: "His Life",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: 2,
+      img: Img2,
+      title: "Who's there",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      id: 3,
+      img: Img3,
+      title: "Lost Boy",
+      description:
+        "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+];
 
 const BestBook = () => {
   return (
@@ -15,6 +42,23 @@ const BestBook = () => {
                     consectetur adipisicing elit. Expedita delectus 
                     impedit recusandae{" "}
                 </p>
+            </div>
+            {/*Card Section*/}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-5 place-items-center">
+                {
+                    BooksData.map((book) => (
+                        <div key={book.id} className="">
+                            <div className="h-[100px]">
+                                <img src={book.img} alt="" />
+                            </div>
+                            <div>
+                                <div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     </div>
