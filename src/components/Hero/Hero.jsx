bb22f0b7825feea9516a450c56ed9c59793fd62c
wacho91 <criskol.71@gmail.com>
2024-10-07 10:18:49 -0500

    Button Hero complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,8 +20,8 @@ const Hero = () => {
                     </p>
                     <div>
                         <button
-                            className="bg-gradient-to-r from-primary to-secondary hover:scale-105 
-                            duration-200 text-white py-2 px-4 rounded-full"
+                            className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-2 rounded-full
+                            mt-4 hover:scale-105 duration-200"
                         >
                             Order Now
                         </button>
