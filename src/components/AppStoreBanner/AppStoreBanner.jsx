274e4a734f5625a996ff207db09056dbdc96cd6a
wacho91 <criskol.71@gmail.com>
2024-10-08 10:59:07 -0500

    Complete AppStore component

--- src/components/AppStoreBanner/AppStoreBanner.jsx
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -1,9 +1,40 @@
 
+import BoardImg from "../../assets/website/board.png"
+import PlayStoreImg from "../../assets/website/play_store (1).png";
+import AppStoreImg from "../../assets/website/app_store (1).png";
+
+const bannerImg = {
+    backgroundImage: `url(${BoardImg})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100%",
+    width: "100%",
+}
 
 const AppStoreBanner = () => {
   return (
-    <div>
-        App Store Banner
+    <div 
+        className="bg-gray-100 dark:bg-gray-800 text-white py-10"
+        style={bannerImg}
+    >
+        <div className="container">
+            <div className="space-y-6 max-w-xl mx-auto">
+                <h1 
+                    className="text-2xl text-center sm:text-4xl font-semibold"
+                >
+                    Read Books at your fingertips
+                </h1>
+                <div className="flex flex-wrap justify-center items-center gap-4">
+                    <a href="#">
+                        <img src={PlayStoreImg} alt="" className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"/>
+                    </a>
+                    <a href="#">
+                        <img src={AppStoreImg} alt="" className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"/>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
