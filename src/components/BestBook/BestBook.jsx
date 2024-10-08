098a765c2fadacf04fb8c15d9db1ed1398bb0daf
wacho91 <criskol.71@gmail.com>
2024-10-08 10:04:54 -0500

    Component complete

--- src/components/BestBook/BestBook.jsx
+++ src/components/BestBook/BestBook.jsx
@@ -67,7 +67,11 @@ const BestBook = () => {
                                 </div>
                                 <h1 className="text-xl font-bold">{book.title}</h1>
                                 <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">{book.description}</p>
-                                <button className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-2 rounded-full mt-4 hover:scale-105 duration-200">Order Now</button>
+                                <button className="bg-primary to-secondary text-white px-4 py-2 
+                                rounded-full mt-4 hover:scale-105 duration-200 group-hover:bg-white group-hover:text-primary"
+                                >
+                                    Order Now
+                                </button>
                             </div>
                         </div>
                     ))
