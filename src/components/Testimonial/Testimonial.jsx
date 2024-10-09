267f56eb5486d9b58c5ae3dad82786a24da1301a
wacho91 <criskol.71@gmail.com>
2024-10-09 10:08:51 -0500

    Changes

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -84,7 +84,7 @@ const Testimonial = () => {
             <div>
                 <Slider {...settings}>
                     {testimonialData.map((data) => (
-                        <div key={data.id}>
+                        <div key={data.id} className="my-6">
                             <div className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative">
                                 <div>
                                     <img src={data.img} alt="" className="rounded-full w-20 h-20 object-cover" />
