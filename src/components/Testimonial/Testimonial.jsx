cbc95b191b9b03a9dfb684b1b6c2e102c09bbed0
wacho91 <criskol.71@gmail.com>
2024-10-09 09:55:53 -0500

    Slide testimonial

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,5 +1,69 @@
 
+import Slider from "react-slick"
+
+const testimonialData = [
+    {
+      id: 1,
+      name: "Victor",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: "https://picsum.photos/101/101",
+    },
+    {
+      id: 2,
+      name: "Satya Narayan",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: "https://picsum.photos/102/102",
+    },
+    {
+      id: 3,
+      name: "Sachin Tendulkar",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: "https://picsum.photos/103/103",
+    },
+];
+
 const Testimonial = () => {
+
+    //Slider config
+    var settings = {
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        // slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: "linear",
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        responsive: [
+          {
+            breakpoint: 10000,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+            },
+          },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              initialSlide: 2,
+            },
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ],
+    };
+
   return (
     <div>
         <div className="container">
@@ -17,7 +81,26 @@ const Testimonial = () => {
             </div>
 
             {/*Card Section*/}
-            
+            <div>
+                <Slider {...settings}>
+                    {testimonialData.map((data) => (
+                        <div key={data.id}>
+                            <div>
+                                <div>
+                                    <img src={data.img} alt="" />
+                                </div>
+                                <div>
+                                    <div>
+                                        <p className="text-gray-50 text-sm">{data.text}</p>
+                                        <h1 className="text-xl font-bold text-black/80 dark:text-white">{data.name}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="">,,</p>
+                        </div>
+                    ))}
+                </Slider>
+            </div>
         </div>
     </div>
   )
